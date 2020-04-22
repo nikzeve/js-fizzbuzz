a2abe3e141cb32e12a1a1d72f85981af71752a07
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-04-22 17:06:17 +0200

    push intermedia

---
+++ main.js
@@ -0,0 +1,4 @@
+// Scrivi un programma che stampi i numeri da 1 a 100, con le seguenti regole:
+// al posto dei multipli di 3 stampi "Fizz"
+// al posto dei multipli di 5 stampi "Buzz"
+// al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
