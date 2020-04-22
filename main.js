def6efe73b80edc3fb532b3adaf6f9db9e7ac02f
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-04-22 20:44:14 +0200

    push intermedia

--- main.js
+++ main.js
@@ -2,3 +2,23 @@
 // al posto dei multipli di 3 stampi "Fizz"
 // al posto dei multipli di 5 stampi "Buzz"
 // al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
+
+for (var i=1; i < 101; i++){
+    if (i % 15 == 0) {
+
+        console.log("FizzBuzz");
+
+    } else if (i % 3 == 0) {
+
+        console.log("Fizz");
+
+    } else if (i % 5 == 0) {
+
+        console.log("Buzz");
+
+    } else {
+
+        console.log(i);
+
+    }
+}
